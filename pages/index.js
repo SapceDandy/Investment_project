704cba90be8ae3237a7042fc6a29cc0144ab7b14
Devon Dudley <Devon.Anthony.Dudley@gmail.com>
2022-09-04 10:18:53 -0500

    Update

--- pages/index.js
+++ pages/index.js
@@ -210,7 +210,7 @@ export default function Home({uploadPosts}) {
     {!user && (
       <div className = "centerWelcomePage">
         <h1>Welcome to Devon's Investment App</h1>
-        <span>This project inspired by the <a style = {{fontWeight: "bold"}} href = "https://next.fireship.io/" target="_blank">Next Fireship</a> project</span>
+        <span>This project was inspired by the <a style = {{fontWeight: "bold"}} href = "https://next.fireship.io/" target="_blank">Next Fireship</a> project - <a style = {{fontWeight: "bold"}} href = "https://github.com/fireship-io/next-firebase-course/tree/main" target="_blank">Next Fireship Repository</a></span>
         <Feed posts = {posts} />
         {(!loading) && (!feedBottom) && (posts?.length !== 0) && (posts.length % numOfPosts === 0) && (<button className = "generalButton" onClick = {getPosts}>More</button>)}
         {(feedBottom) && (posts?.length !== 0) && (posts.length % numOfPosts !== 0) && (<span>You have reached the end!</span>)}
